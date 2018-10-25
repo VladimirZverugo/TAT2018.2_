65d417e00849b70545051c6d84ceaaa45c8abd84
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2018-10-25 16:42:54 +0300

    3d Task

---
+++ TranscoderMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DEV3;
+using System.Text;
+
+
+
+namespace DEV3
+{
+    class MainClass
+    {   
+        static void Main(string[] args)
+        {
+            int num; // takes Number in 10th scale of notation
+            int numBase; // new scale of notation
+            BaseConverter mda = new BaseConverter();
+
+            try
+            {
+                Console.WriteLine("input symbols");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("input your base");
+                numBase = Convert.ToInt32(Console.ReadLine());
+                mda.Converter(num, numBase);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Some Exception. SORRY!!! {0}",e);
+            }
+        }
+    }
+}
