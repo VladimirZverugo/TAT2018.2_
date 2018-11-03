896518649f5e97fd03a1e590e759799bf5293a2f
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2018-11-03 10:09:04 +0300

    DEV-3 Task (Scales of notation [2-20]) (#7)
    * 3d Task
    
    * Update TranscoderBody.cs
    
    * Update TranscoderBody.cs
    
    * Update TranscoderMain.cs

---
+++ TranscoderBody.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DEV3
+{
+    public class BaseConverter
+    {
+        /// <summary>
+        /// Converter: Number from 10th scale of notation to new base scale
+        /// Reverse: inverts answer string to it's right form
+        /// </summary>
+
+        static string baseNum = "0123456789ABCDEFGHIJ";
+
+        public void Converter(int num, int numBase)
+        {
+            string answer = "";
+
+            while (num >= numBase)
+            {
+                answer += baseNum[num % numBase];
+                num /= numBase;
+            }
+            answer += baseNum[num];
+            answer = Reverse(answer);
+            Console.WriteLine(answer);
+        }
+
+        private string Reverse(string num)
+        {
+            char[] charArray = num.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }  
+    }
+}
