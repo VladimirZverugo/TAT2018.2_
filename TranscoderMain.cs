84f506ffba1c5a6c8a5452c0ac106cd4b28f4914
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2018-11-03 10:13:00 +0300

    DEV-3 Task (Scales of notation [2-20]) (#7) (#10)
    * 3d Task
    
    * Update TranscoderBody.cs
    
    * Update TranscoderBody.cs
    
    * Update TranscoderMain.cs

---
+++ TranscoderMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DEV3
+{
+    class MainClass
+    {   
+        static void Main(string[] args)
+        {
+            int num; // takes Number in 10th scale of notation
+            int numBase; // new scale of notation
+            BaseConverter mda = new BaseConverter();
+
+            try
+            {
+                Console.WriteLine("input symbols");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("input your base");
+                numBase = Convert.ToInt32(Console.ReadLine());
+                mda.Converter(num, numBase);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Some Exception. SORRY!!! {0}",e);
+            }
+        }
+    }
+}
